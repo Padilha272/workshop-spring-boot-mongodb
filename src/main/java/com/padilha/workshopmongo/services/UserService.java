f821eb37acef21c3afe6316383e4c812351e1648
Matheus de Araújo Padilha <mapadilha02@gmail.com>
2023-10-05 15:52:08 -0300

    Connection with MongoDB using repository and service

---
+++ src/main/java/com/padilha/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.padilha.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.padilha.workshopmongo.domain.User;
+import com.padilha.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
