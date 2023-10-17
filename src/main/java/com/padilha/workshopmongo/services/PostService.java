3ded324fc59b89bbfb92f6b68be2efbdf31f56f7
Matheus de Araújo Padilha <mapadilha02@gmail.com>
2023-10-17 16:07:11 -0300

    Query with mutiple criteria

--- src/main/java/com/padilha/workshopmongo/services/PostService.java
+++ src/main/java/com/padilha/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.padilha.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,6 +31,10 @@ public class PostService {
 		return repo.searchTitle(text);
 	}
 	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate ){
+		maxDate = new Date(maxDate.getTime()+24*60*60*1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 	
 	
 }
